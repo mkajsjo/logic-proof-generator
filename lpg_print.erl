f97d3aa491fc26ab8b2f21bc1ae1999593bbe29c
Mikael Kajsjo <mkajsjo@gmail.com>
2020-11-20 13:12:20 +0100

    print assumption boxes

--- lpg_print.erl
+++ lpg_print.erl
@@ -13,21 +13,179 @@ print_proof(Proof) ->
     io:fwrite(proof_to_iolist(Proof)).
 
 
-proof_to_iolist([]) ->
+proof_to_iolist(Proof) ->
+    proof_to_iolist(Proof, calc_proof_paddings(Proof)).
+
+
+proof_to_iolist([], _) ->
     [];
-proof_to_iolist([{Ref, Expr, Rule, Refs} | Ps]) ->
+proof_to_iolist(
+    [{Ref, Expr, Rule, Refs} | Ps],
+    #{row_nr_size := RS, exprs_size := ES, open_boxes := OB, inner_boxes := IB} = Map0
+) ->
+    OpenBoxes = open_or_close_box(Rule, OB),
+    InnerBoxes =
+        case Rule of
+            assumption ->
+                [find_nr_inner_boxes(Ps) | IB];
+            R when R =:= '->i'; R =:= '!i'; R =:= 'PBC' ->
+                tl(IB);
+            _ ->
+                IB
+        end,
+    Map =
+        Map0#{
+            open_boxes => OpenBoxes,
+            inner_boxes => InnerBoxes
+        },
     [
-        string:pad(integer_to_binary(Ref), 2, trailing), %TODO don't pad fixed length
+        close_assumption_box(Rule, Map0),
+        open_assumption_box(Rule, Map),
+        string:pad(integer_to_binary(Ref), RS, trailing),
         <<"   ">>, %TODO other spacer?
-        string:pad(expr_to_string(Expr), 16, trailing), %TODO don't pad fixed length
+        assumption_boxes_vertical(Map, leading),
+        string:pad(expr_to_string(Expr), ES, trailing),
+        assumption_boxes_vertical(Map, trailing),
         <<"   ">>, %TODO other spacer?
-        atom_to_binary(Rule, utf8), %TODO pad, custom?
+        string:pad(atom_to_binary(Rule, utf8), 3, trailing),
         <<" ">>,
-        lists:join(<<",">>, [integer_to_binary(R) || R <- Refs]), %TODO range when closing assumption
+        format_references(Rule, Refs),
         <<"\n">>
     |
-        proof_to_iolist(Ps)
+        proof_to_iolist(Ps, Map)
     ].
+
+
+calc_proof_paddings(Proof) ->
+    calc_proof_paddings(
+        Proof,
+        #{
+            row_nr_size => 0,
+            open_boxes => 0,
+            max_boxes => 0,
+            exprs_size => 0
+        }
+    ).
+
+
+calc_proof_paddings([], Acc) ->
+    Acc#{inner_boxes => [0]};
+calc_proof_paddings([{Ref, Expr, Rule, _} | Ps], Map) ->
+    #{
+        open_boxes := OpenBoxes0,
+        max_boxes := MaxBoxes,
+        exprs_size := ExprsSize
+    }
+        = Map,
+    OpenBoxes = open_or_close_box(Rule, OpenBoxes0),
+    calc_proof_paddings(
+        Ps,
+        #{
+            row_nr_size => string:length(integer_to_binary(Ref)),
+            open_boxes => OpenBoxes,
+            max_boxes => max(OpenBoxes, MaxBoxes),
+            exprs_size => max(string:length(expr_to_string(Expr)), ExprsSize)
+        }
+    ).
+
+
+open_or_close_box(assumption, OpenBoxes) ->
+    OpenBoxes + 1;
+open_or_close_box(Rule, OpenBoxes) when Rule =:= '->i'; Rule =:= '!i'; Rule =:= 'PBC' ->
+    OpenBoxes - 1;
+open_or_close_box(_, OpenBoxes) ->
+    OpenBoxes.
+
+
+find_nr_inner_boxes(Proofs) ->
+    find_nr_inner_boxes(Proofs, 0, 0).
+
+
+find_nr_inner_boxes([], _, NrInnerBoxes) ->
+    NrInnerBoxes;
+find_nr_inner_boxes(_, -1, NrInnerBoxes) ->
+    io:fwrite("NrInnerBoxes: ~p~n", [NrInnerBoxes]),
+    NrInnerBoxes;
+find_nr_inner_boxes([{_, _, assumption, _} | Ps], Counter, NrInnerBoxes) ->
+    find_nr_inner_boxes(
+        Ps,
+        Counter + 1,
+        max(NrInnerBoxes, Counter + 1)
+    );
+find_nr_inner_boxes([{_, _, Rule, _} | Ps], Counter, NrInnerBoxes) when Rule =:= '->i'; Rule =:= '!i'; Rule =:= 'PBC' ->
+    find_nr_inner_boxes(
+        Ps,
+        Counter - 1,
+        NrInnerBoxes
+    );
+find_nr_inner_boxes([_ | Ps], Counter, NrInnerBoxes) ->
+    find_nr_inner_boxes(Ps, Counter, NrInnerBoxes).
+
+
+open_assumption_box(assumption, Map) ->
+    assumption_box_horizontal(Map);
+open_assumption_box(_, _) ->
+    <<>>.
+
+
+close_assumption_box(Rule, Map) when Rule =:= '->i'; Rule =:= '!i'; Rule =:= 'PBC' ->
+    assumption_box_horizontal(Map);
+close_assumption_box(_, _) ->
+    <<>>.
+
+
+assumption_box_horizontal(
+    #{
+        row_nr_size := RowNrSize,
+        exprs_size := ExprsSize,
+        inner_boxes := [InnerBoxes | _],
+        open_boxes := OpenBoxes,
+        max_boxes := MaxBoxes
+    }
+) ->
+    [
+        lists:duplicate(RowNrSize + 3, <<" ">>), %TODO magic number
+        string:pad(
+            unicode:characters_to_binary(
+                lists:duplicate(OpenBoxes - 1, <<"|">>)
+                ++ [<<"┼"/utf8>> | lists:duplicate(InnerBoxes, <<"-">>)]
+            ),
+            MaxBoxes,
+            leading
+        ),
+        lists:duplicate(ExprsSize + InnerBoxes, <<"-">>),
+        <<"┼"/utf8>>,
+        lists:duplicate(OpenBoxes - 1, <<"|">>),
+        <<"\n">>
+    ].
+
+
+assumption_boxes_vertical(#{open_boxes := O, max_boxes := M, inner_boxes := [I|_]}, PadDirection) ->
+    Foo =
+        case PadDirection of
+            leading ->
+                lists:duplicate(O, <<"|">>) ++ lists:duplicate(I, <<" ">>);
+            trailing ->
+                lists:duplicate(I, <<" ">>) ++ lists:duplicate(O, <<"|">>)
+        end,
+    string:pad(
+        unicode:characters_to_binary(Foo),
+        M,
+        PadDirection
+    ).
+
+
+format_references(Rule, [A, B]) when Rule =:= '->i'; Rule =:= '!i'; Rule =:= 'PBC' ->
+    [
+        integer_to_binary(A),
+        <<"-">>,
+        integer_to_binary(B)
+    ];
+format_references(_, Refs) ->
+    lists:join(
+        <<",">>,
+        [integer_to_binary(R) || R <- Refs]
+    ).
 
 
 expr_to_string({'!', {_, _, _} = Expr}) ->
